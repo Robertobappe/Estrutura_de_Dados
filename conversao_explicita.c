c62b564d3abeed9f45eef69fced3220be0876206
Roberto Silva <90939196+Robertobappe@users.noreply.github.com>
2022-08-25 10:55:07 -0300

    Add files via upload

---
+++ conversao_explicita.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    double x = 1.2;
+    // Conversão explicita de double para int
+    int sum = (int)x + 1;
+    printf("sum = %d", sum);
+    return 0;
+}
+
+
