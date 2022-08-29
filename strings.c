e17a4d91251500798f1a25702491f86c643f0cc3
Roberto Silva <90939196+Robertobappe@users.noreply.github.com>
2022-08-29 20:26:48 -0300

    Add files via upload

---
+++ strings.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char str[] = "string";
+    printf("String length: %lu\n", strlen(str)); //6
+    printf("String size: %lu\n", sizeof(str)); //7
+}
+
